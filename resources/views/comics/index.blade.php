9b95a457259312d1b05a8bcb2d0ec70d18e082c7
Andrea Baronchelli <baronchelli.andrea@gmail.com>
2021-06-14 15:44:14 +0200

    add comics index

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.main')
+
+@section('content')
+<div class="container">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Type</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($comics as $comic) 
+            <tr>
+                <td>{{ $comic->title }}</td>
+                <td>{{ $comic->type }}</td>
+                <td>
+                    <a href="#">SHOW</a>
+                </td>
+            </tr> 
+            @endforeach
+        </tbody>
+    </table>
+</div>
+    
+@endsection
