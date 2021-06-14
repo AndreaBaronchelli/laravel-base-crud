b14095cd9adfcd02c1466dede0ef20c9508db155
Andrea Baronchelli <baronchelli.andrea@gmail.com>
2021-06-14 15:20:00 +0200

    init repo

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat consectetur obcaecati perferendis quod? Accusamus dolores aperiam quo voluptas quaerat iure omnis veritatis, in ratione magni ex a deserunt reiciendis perferendis?
+    </div>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        {{-- BOOTSTRAP --}}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+        <title>Comics</title>
+
+        
+    </head>
+
+    <body>
+        @include('partials.header')
+
+        <main>
+            @yield('content')
+        </main>
+
+        @include('partials.footer')
+    </body>
+   
+</html>
